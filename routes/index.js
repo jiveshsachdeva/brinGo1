b13cf27d5c20393595aee248727b522241cc5ac4
Abhishek chaudhary <32256891+abhishek07456@users.noreply.github.com>
2018-10-02 22:12:57 +0530

    Add files via upload

--- routes/index.js
+++ routes/index.js
@@ -4,26 +4,131 @@ var router = express.Router();
 /* GET home page. */
 
 
-router.get('/', function(req, res, next) {
-  res.render('template', { title: 'Express' });
-});
-router.get('/student', function(req, res, next) {
-  res.render('student', { title: 'Express' });
-});
-router.get('/library', function(req, res, next) {
-  res.render('library', { title: 'Express' });
-});
-router.get('/teacher', function(req, res, next) {
-  res.render('teacher', { title: 'Express' });
-});
-router.get('/user', function(req, res, next) {
-  res.render('user', { title: 'Express' });
-});
-router.get('/table', function(req, res, next) {
-  res.render('table', { title: 'Express' });
-});
-router.get('/usertype', function(req, res, next) {
-  res.render('usertype', { title: 'Express' });
+router.get('/bringo', function(req, res,next) {
+  res.render('bringo', { title: 'Express' });
+next();
 });
 
+router.get('/bringo/userprofile', function(req, res,next) {
+ 
+  res.render('user', { title: 'Express' });
+
+next();
+});
+
+router.get('/bringo/user', function(req, res,next) {
+ 
+  res.render('usertype', { title: 'Express' });
+
+next();
+});
+router.get('/bringo/libraryrecord', function(req, res,next) {
+ 
+  res.render('record', { title: 'Express' });
+
+next();
+});
+router.get('/bringo/login', function(req, res,next) {
+ 
+  res.render('login', { title: 'Express' });
+
+next();
+});
+router.get('/bringo/forgetpassword', function(req, res,next) {
+ 
+  res.render('forget', { title: 'Express' });
+
+next();
+});
+//user parts
+
+
+router.get('/bringo/user/admin', function(req, res,next) {
+ 
+  res.render('admin', { title: 'Express' });
+
+next();
+});
+
+router.get('/bringo/user/teacher', function(req, res,next) {
+ 
+  res.render('teacher', { title: 'Express' });
+
+next();
+});
+router.get('/bringo/user/student', function(req, res,next) {
+ 
+  res.render('student', { title: 'Express' });
+
+next();
+});
+router.get('/bringo/user/library', function(req, res,next) {
+ 
+  res.render('library_staff', { title: 'Express' });
+
+next();
+});
+router.get('/bringo/user/alumnistudent', function(req, res,next) {
+ 
+  res.render('alumni', { title: 'Express' });
+
+next();
+});
+//issue & feedback
+router.get('/bringo/user/student/feedback', function(req, res,next) {
+ 
+  res.render('formpage', { title: 'Express' });
+
+next();
+});
+router.get('/bringo/user/student/issuebook', function(req, res,next) {
+ 
+  res.render('frontpage', { title: 'Express' });
+
+next();
+});
+router.get('/bringo/user/teacher/feedback', function(req, res,next) {
+ 
+  res.render('formpage', { title: 'Express' });
+
+next();
+});
+router.get('/bringo/user/teacher/issuebook', function(req, res,next) {
+ 
+  res.render('frontpage', { title: 'Express' });
+
+next();
+});
+router.get('/bringo/user/teacher/uploadnotes', function(req, res,next) {
+ 
+  res.render('upload', { title: 'Express' });
+
+next();
+});
+router.get('/bringo/user/student/uploadnotes', function(req, res,next) {
+ 
+  res.render('upload', { title: 'Express' });
+
+next();
+});
+
+router.get('/bringo/user/student/downloadnotes', function(req, res,next) {
+ 
+  res.render('upload', { title: 'Express' });
+
+next();
+});
+
+router.get('/bringo/user/student/bookstatus', function(req, res,next) {
+ 
+  res.render('user', { title: 'Express' });
+
+next();
+});
+router.get('/bringo/user/teacher/bookstatus', function(req, res,next) {
+ 
+  res.render('user', { title: 'Express' });
+
+next();
+});
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -2,35 +2,11 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/frontpage', function(req, res, next) {
+router.get('/record', function(req, res, next) {
 //  res.send('respond with a resource');
 
-  res.render('frontpage.html');
+  res.render('record.html');
   
 });
-router.get('/formpage', function(req, res, next) {
-  //  res.send('respond with a resource');
-  
-    res.render('formpage.html');
-    
-  });
 
-  router.get('/table', function(req, res, next) {
-    //  res.send('respond with a resource');
-    
-      res.render('table.html');
-      
-    });
-    router.get('/user', function(req, res, next) {
-      //  res.send('respond with a resource');
-      
-        res.render('user.html');
-        
-      });
-      router.get('/usertype', function(req, res, next) {
-        //  res.send('respond with a resource');
-        
-          res.render('usertype.html');
-          
-        });
 module.exports = router;
